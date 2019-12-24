614ae2e9d7af6a7d17bf87e8651ff471a5947bf0
Your Name <you@example.com>
2019-12-23 22:48:59 -0800

    u

---
+++ src/api/yelp.js
@@ -0,0 +1,15 @@
+const request = require('request'); // npm i request -s
+const config = require('./config.json');
+
+request.get( config.yelp.businesses, {
+    auth: {
+      bearer: config.yelp.apiKey,
+    },
+  }, ( error, response, body, ) => {
+    if (error) {
+      return console.error( 'Upload failed:', error, );
+    }
+    console.log( 'response', response, );
+    console.log( 'body', body, );
+  }
+);
--- src/spiders/singles/templateList.js
+++ src/spiders/singles/templateList.js
@@ -92,37 +92,21 @@ const run = () =>
     try {
       const browser = await puppeteer.launch(options);
       const page = await browser.newPage();
-      // const searchTerm = process.argv[2]; // node scraper iphone
-      // await page.goto('https://www.croma.com/');
-      // await page.click('button.mobile__nav__row--btn-search');
-      // await page.type('input#js-site-search-input', searchTerm);
-      // await page.keyboard.press('Enter');
-      // await page.screenshot({ path: 'sample.png', });
       await page.goto(config.url);
-      // await page.goto('https://www.yellowpages.com/search?search_terms=Real+Estate+Agents&geo_location_terms=High+Point%2C+NC');
       const pageData = await page.evaluate( config => {
         const { selectors, } = config;
         const { items: itemsSelector, item: itemConfig, } = selectors;
         const results = [];
         const items = document.querySelectorAll(itemsSelector);
-        // const items = document.querySelectorAll('div.info');
         items.forEach( item => {
           const newData = {};
-
-          // const price = item.querySelector('span.pdpPrice').innerText
-          // const discount = item.querySelector('div.listingDiscnt').innerText
-          // const link = item.querySelector('a').href
-          // const link = item.querySelector(itemSelector.link).href
           for (const property in itemConfig) {
-            // console.log(`${property}: ${itemSelector[property]}`);
             try {
               newData[property] = item.querySelector(itemConfig[property].selector)[itemConfig[property].attribute] 
             } catch (e) {
               console.log(e);
             }
           }
-
-          // results.push({ link, });
           results.push(newData);
         });
         return results;
