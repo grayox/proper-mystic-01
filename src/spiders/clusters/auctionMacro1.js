c623dfdf2d75da495651f5121b8d55785c5fdca1
Your Name <you@example.com>
2020-01-13 00:17:52 -0800

    u

--- src/spiders/clusters/auctionMacro1.js
+++ src/spiders/clusters/auctionMacro1.js
@@ -8,131 +8,83 @@
 // You need to download the Alexa 1M from http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
 // and unzip it into this directory
 
+// const puppeteer = require('puppeteer'); // npm i puppeteer -s
 const { Cluster } = require('puppeteer-cluster');
-const puppeteer = require('puppeteer'); // npm i puppeteer -s
+const _ = require('lodash');
 
 const auctionList = require('./auctionList');
-const states = require('../../lib/geoData/states.json');
-const isScheduled = require('../../util/scheduler');
+const getStatesList = require('../../lib/geoData/getStatesList.js');
+// const isScheduled = require('../../util/scheduler');
 
-// const getDb = require('../../lib/db/getDb');
-// const db = getDb();
+const getDb = require('../../lib/db/getDb');
+const todayDate = require('../../util/todayDate');
+const write2db = require('../../lib/db/write2firestore');
+
+// increment counters
+// ref: https://firebase.google.com/docs/firestore/manage-data/add-data#increment_a_numeric_value
+// ref: https://fireship.io/snippets/firestore-increment-tips/
+const admin = require('firebase-admin');
+const incrementer = admin.firestore.FieldValue;
 
 // start here
-const scriptName = 'auctionMacro';
+const scriptName = 'auctionMacro'; // node auctionMacro
 // calls auctionList.js
 
+const MAX_STATES_PER_RUN = 1;
+const MAX_PAGES_PER_STATE_PER_RUN = 5; // 1;
 const MAX_CONCURRENCY = 5;
-const options = { 
-  slowMo: 1000,
+const puppeteerOptions = { 
+  // slowMo: 1000,
   // headless: false,
+  // enable heroku // ref: https://stackoverflow.com/a/55090914
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ],
+};
+const stateInitObject = {
+  isActive: true, // all states not yet scraped today
+  isCurrent: false, // current state to scrape
+  pagesAttempted: 0, // total pages attempted to be scraped
+  pagesCount: 0, // total count of pages with data we successfully captured
+  itemsCount: 0, // total number of items captured from pages with data
+  currentItems: 0, // total number of captured items that are current
 };
 
-const getDbInventory = db => {
+// do not change
+const PAGE_INCREMENT = 1;
 
-  // date calculations
-  const maxDaysToGoBack = 5;
-  const milSecMultiplier = 1000 * 60 * 60 * 24;
-  const timestamp = Date.now();
-  // timestamp
-  const offset = maxDaysToGoBack * milSecMultiplier;
-  // offset
-  const minDate = timestamp - offset;
-  // minDate
-  
-  // const collection = 'markets';
-  // const doc = 'us-va-richmond';
-  const queryFilter = {
-    collection: 'inventory',
-    limit: 25,
-    filters: [
-      // // {
-      // //   field: 'isTest',
-      // //   operator: '==',
-      // //   value: true,
-      // // },
-      // {
-      //   field: 'isActive',
-      //   operator: '==',
-      //   value: true,
-      // }, {
-      //   field: 'timestamp',
-      //   operator: '>',
-      //   value: minDate,
-      // },
-    ],
-  }
+const db = getDb();
+const { formattedDate, } = todayDate;
+const stateAbbreviations = getStatesList('abbreviation');
+const getStatesInit = () => {
+  const out = {};
+  stateAbbreviations.forEach( state => out[state] = stateInitObject );
+  return out;
+};
 
-  // fetching a list from a doc
-  // // [START] fetch data
-  // // ref: https://firebase.google.com/docs/firestore/query-data/get-data#get_a_document
-  // const marketRef = db
-  //   .collection(collection)
-  //   .doc(doc);
-  // const domains = await marketRef.get()
-  //   .then( doc => {
-  //     if (!doc.exists) {
-  //       console.log('No such document!');
-  //     } else {
-  //       const data = doc.data();
-  //       // console.log('Document data:', data,);
-  //       const { domainList, } = data;
-  //       return domainList;
-  //     }
-  //   })
-  //   .catch(err => {
-  //     console.log('Error getting document', err);
-  //   });
-  // // console.log('domains', domains,);
-  // // return;
-  // // [END] fetch data
+const dbConfig = {
+  source: 'auction',
+  stats: {
+    collection: 'stats',
+    doc: formattedDate,
+  },
+  states: {
+    collection: 'states',
+    doc: formattedDate,
+  },
+};
 
-  // fetching a collection subset with a query
-  // [START] fetch data
-  // ref: https://firebase.google.com/docs/firestore/query-data/get-data#get_multiple_documents_from_a_collection
-  // const { collection, field, operator, value, limit, } = queryFilter;
-  const { collection, filters, limit, } = queryFilter;
-  const collectionRef = db.collection(collection);
-  const query = collectionRef
-    // // .where('capital', '==', true)
-    // // .where( 'hasContactUrls', '==', false, )
-    // // .where( field, operator, value, )
-    // .where( filters[0].field, filters[0].operator, filters[0].value, )
-    // .where( filters[1].field, filters[1].operator, filters[1].value, )
-    // // .where( filters[2].field, filters[2].operator, filters[2].value, )
-    // // // order and limit data
-    // // // https://firebase.google.com/docs/firestore/query-data/order-limit-data#order_and_limit_data
-    // // .orderBy('name')
-    // // .limit(3)
-    .limit(limit)
-    .get()
-    .then( snapshot => {
-      if (snapshot.empty) {
-        console.log('No matching documents.');
-        return;
-      }
-      const out = [];
-      snapshot.forEach( doc => {
-        // console.log( doc.id, '=>', doc.data() );
-        out.push( doc.data() );
-      });
-      // const out = snapshot.map( doc => doc.data() ); // .map() not a function
-      return out;
-    })
-    .catch( err => {
-      console.log('Error getting documents', err,);
-    });
-    // console.log( 'query', query, );
-    // console.log( 'query', JSON.stringify(query, null, 2,), );
-    // return;
-  // [END] fetch data
-  return query;
+const getRandomState = subsetOfStates => {
+  const keys = Object.keys( subsetOfStates );
+  const keysLength = keys.length;
+  const keysAvail = keysLength - 1;
+  const targetKey = _.random( keysAvail );
+  const out = keys[ targetKey ];
+  return out; // 'WV'
 }
 
-(async () => {
-  // schedule it
-  // if(!isScheduled(scriptName)) return;
-
+const runCluster = async statesInQueue => {
   // // ref: https://github.com/GoogleChrome/puppeteer
   // // cheatsheet: https://nitayneeman.com/posts/getting-to-know-puppeteer-using-practical-examples/
   // // forms: https://stackoverflow.com/questions/45778181/puppeteer-how-to-submit-a-form
@@ -143,7 +95,7 @@ const getDbInventory = db => {
     maxConcurrency: MAX_CONCURRENCY,
     monitor: true,
     // timeout: 30000, // 30000 default
-    // puppeteerOptions: slowDown,
+    puppeteerOptions,
   });
 
   // In case of problems, log them
@@ -151,21 +103,82 @@ const getDbInventory = db => {
     console.log(`Error crawling ${data}: ${err.message}`);
   });
   
-  const inventory = null; // await getDbInventory(db);
-  
-  const { asArray: statesArray, } = states;
-  const length = statesArray.length;
-  // const i = 15;
-  // let i = length;
-  let i = 15; while(i--) {
-    const state = statesArray[i];
-    const { variant, abbreviation, } = state;
-    const item = { inventory, abbreviation, };
-    // console.log('variant', variant,);
-    // console.log('abbreviation', abbreviation,);
-    if ( variant === 'state' ) cluster.queue( item, auctionList, ); // ref: https://github.com/thomasdondorf/puppeteer-cluster/blob/master/examples/function-queuing-complex.js
+  const keys = Object.keys( statesInQueue );
+  let i = MAX_STATES_PER_RUN; while(i--) {
+    const state = keys[i]; // 'WV'; // 'NY'; // 
+    const pageNumber = statesInQueue[state].pagesAttempted + PAGE_INCREMENT;
+    const jMax = pageNumber + MAX_PAGES_PER_STATE_PER_RUN;
+    for( j = pageNumber; j < jMax; j++ ) {
+      const item = { state, pageNumber: j, };
+      console.log('item', item,);
+      // ref: https://github.com/thomasdondorf/puppeteer-cluster/blob/master/examples/function-queuing-complex.js
+      cluster.queue( item, auctionList, );
+    }
   }
 
   await cluster.idle();
   await cluster.close();
+}
+
+( async () => {
+  // schedule it
+  // if(!isScheduled(scriptName)) return;
+
+  const statesRef = db.collection('states').doc(formattedDate);
+  const initDoc = () => {
+    const statesInit = getStatesInit();
+    statesRef.set(statesInit);
+    console.log( "Initialized new 'states' for a new day!", formattedDate, );
+    // // set queue: list of states not yet scraped
+    // // console.log( 'stateAbbreviations', stateAbbreviations, );
+    // statesRef.set({ inQueue: stateAbbreviations, });
+  }
+  
+  db.runTransaction( t => {
+    return t.get( statesRef )
+      .then( doc => {
+        // // Add one person to the city population.
+        // // Note: this could be done without a transaction
+        // //       by updating the population using FieldValue.increment()
+        // let newPopulation = doc.data().population + 1;
+        // t.update(statesRef, {population: newPopulation});
+        // node auctionMacro
+        const data = doc.data();
+        // console.log( 'data', data, );
+        // if( !(data && ( 'AL' in data ))) initDoc();
+        if( data === undefined ) initDoc();
+        else {
+          // determine which states need to be run
+          
+          // active
+          const activeStates = _.pickBy( data, 'isActive', );
+          if(_.isEmpty(activeStates)) return;
+          
+          // current
+          const currentStates = _.pickBy( activeStates, 'isCurrent', );
+          if(_.isEmpty(currentStates)) {
+            const newCurrentState = getRandomState(activeStates);
+            const states = {};
+            states[newCurrentState] = {
+              isCurrent: true,
+            };
+            const stats = {
+              statesAttempted: incrementer.increment(1),
+            }
+            write2db({ dbConfig, data: { states, stats, }, });
+            console.log('Updated new current state:', newCurrentState,);
+            return;
+          };
+
+          // run those states
+          console.log( 'currentStates', currentStates, );
+          runCluster( currentStates );
+        }
+      });
+  }).then( result => {
+    console.log('Transaction success!');
+  }).catch( err => {
+    console.log('Transaction failure:', err);
+  });
+  
 })();
